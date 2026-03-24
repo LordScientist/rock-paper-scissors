d1a54b18f0584d38b909871f0f267f8a68d04c08
LordScientist <rolandquarshie10@gmail.com>
2026-03-24 21:35:45 +0000

    update branch

--- script.js
+++ script.js
@@ -1,142 +1,76 @@
 // Algorithm
-// Design playermoves
-let playerChoice;
-const game = document.querySelector('.game');
-game.addEventListener('click',playerMove);
+// Design computer movement logic
 
-
-function playerMove(move)
-    {
-        
-        if (trial > 4)
-            {
-                alert("You have exhausted your attempts")
-                let rematch = prompt("Would you like to play again? Enter 'Yes or NO' to proceed")
-                if (rematch === 'Y'|| rematch === 'y'|| rematch === 'Yes'  || rematch === 'yes')
-                    {
-                        trial = 0;
-                                                
-                    }
-                
-
-
-                else
-                    {
-                        alert("Comeback anytime");
-                        total() += scoreboard();
-
-                        return;
-                    }
-
-                
-
-            }        
-
-        else if (move.target === document.querySelector('.rock'))
-            {
-                playerChoice = 'rock';
-                
-            }
-        else if (move.target === document.querySelector('.paper'))
-            {
-                playerChoice = 'paper';
-            }
-        else if (move.target === document.querySelector('.scissors'))
-            {
-                playerChoice = 'scissors'
-            }
-        else 
-            {
-                alert('Choose any of the options')
-                return;
-            }
-        judge();
-        trial+=1
-
-
-    }
-
-
-
-// Design computermoves
-
-function computerMove()
+function computer()
     {
         const move = Math.random();
-        if (move < 0.3)
+        if (move <= 0.3)
             {
-                return 'rock';
-            }
-        else if (move >= 0.3 && move <0.6)
+                return "rock";
+            }    
+
+        else if (move > 0.3 && move <= 0.6)
             {
-                return 'paper';
+                return "paper";
             }
+
         else 
             {
-                return 'scissors';
+                return "scissors";
             }
+
     }
-// Design logic for win,loss and draw
 
-const result = document.querySelector('.result');
-const scoreboard = document.querySelector('.scoreboard');
+// Design player movement logic
+const game = document.querySelector('.game');
+game.addEventListener('click',player)
 
-
-function judge()
+function player(move)
     {
-        const computerChoice = computerMove();
+        let playerMove;
         
-        if (playerChoice === computerChoice)
-                {
-                    result.innerHTML = "A stalemate";
-                }
-        else if (playerChoice === 'rock' && computerChoice === 'scissors' 
-            || playerChoice === 'paper' && computerChoice === 'rock' 
-            || playerChoice === 'scissors' && computerChoice === 'paper')
-                {
-                    result.innerHTML = "You win";
-                }
-        else 
-                {
-                    result.innerHTML = "You Lost";
-                }
+        if (move.target === document.querySelector('.selectRock'))
+            {
+                playerMove = 'rock';
+            }
+            
+        else if (move.target === document.querySelector('.selectPaper'))
+            {
+                playerMove = 'paper';
+            }
 
-        function score()
-        {
-            if (playerChoice === computerChoice)
+        else if (move.target === document.querySelector('.selectScissors'))
+            {
+                playerMove = 'scissors';
+            }
+
+        if (playerMove)
                 {
-                    draw+=1
+                    const computerMove = computer();
+                    determineWinner(playerMove, computerMove);
                 }
-            else if (playerChoice === 'rock' && computerChoice === 'scissors' 
-            || playerChoice === 'paper' && computerChoice === 'rock' 
-            || playerChoice === 'scissors' && computerChoice === 'paper')
-                {
-                    win+=1
-                }         
-                
-            else
-                {
-                    loss+=1
-                }
-        }
-        score()
-        scoreBoard()
-                    
     }
 
-let trial = 0
 
-let win = 0;
-let draw = 0;
-let loss = 0;
+// Design win,loss or draw logic
+const display = document.querySelector('.immediateResult');
 
-
-
-function scoreBoard()
+function determineWinner(playerMove, computerMove)
     {
-        scoreboard.innerHTML = `Win : ${win}
-                                Loss : ${loss}
-                                Draw : ${draw}`
+        if (playerMove === computerMove)
+            {
+                display.innerText = "Stalemate";
+            }
+        else if (
+            (playerMove === 'rock' && computerMove === 'scissors') ||
+            (playerMove === 'paper' && computerMove === 'rock') ||
+            (playerMove === 'scissors' && computerMove === 'paper')
+        )
+            {
+                display.innerText = "You win";
+            }
+        else
+            {
+                display.innerText = "You lost";
+            }
     }
-
-scoreBoard()
